de8470e6422a7112ec6a45032caf53fe647327f9
Joselito <eu@joselitojunior.com>
2020-06-12 02:11:03 -0300

    v0

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,52 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Joseli.to</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Joselito Júnior</title>
+        <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="/mono.css" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Joseli.to
-        </h1>
+        <img src="/logo-white.svg" alt="Jose" className="logo" />
+        <code>$ npx <strong>joselito</strong> {/*--lang=pt-br*/}</code>
+
+        <p className="description">
+        I am a software engineer, consultant, and public speaker currently based in Rio de Janeiro, Brazil. My interests range from technology to entrepreneurship. I love travelling, web development, and burritos.
+        </p>
+
+       {
+         /*
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>.dev &rarr;</h3>
+            <p>.</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+          >
+            <h3>.blog &rarr;</h3>
+            <p></p>
+          </a>
+        </div>
+        */
+       }
+
+        <ul className="social">
+          <li><a href="http://twitter.com/breakzplatorm" className="social__link" rel="noopnener">Twitter</a></li>
+          <li><a href="http://github.com/breakzplatorm" className="social__link" rel="noopnener">GitHub</a></li>
+          <li><a href="http://dev.to/joselito" className="social__link" rel="noopnener">DEV</a></li>
+          <li><a href="http://notes.jose" className="social__link" rel="noopnener">Hashnode</a></li>
+          <li><a href="http://twitter.com/breakzplatorm" className="social__link" rel="noopnener">Unsplashed</a></li>
+          <li><a href="http://twitter.com/breakzplatorm" className="social__link" rel="noopnener">PicPay</a></li>
+        </ul>
+
       </main>
+
+      <footer>
+        CC-BY 2010-2020 Joselito Júnior
+      </footer>
 
       <style jsx>{`
         .container {
@@ -31,6 +68,25 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          max-width: 800px;
+        }
+
+        .logo {
+          margin: 0 0 2em;
+        }
+
+        .social {
+          list-style: none;
+          padding: 0;
+        }
+
+        .social li {
+          display: inline-block;
+          margin: 0.75rem;
+        }
+
+        .social__link {
+          border-bottom: 2px solid #000;
         }
 
         footer {
@@ -58,7 +114,7 @@ export default function Home() {
         }
 
         .title a {
-          color: #0070f3;
+          color: #2D00F7;
           text-decoration: none;
         }
 
@@ -80,8 +136,9 @@ export default function Home() {
         }
 
         .description {
-          line-height: 1.5;
+          line-height: 1.618;
           font-size: 1.5rem;
+          margin: 2em 0 3rem;
         }
 
         code {
@@ -89,8 +146,7 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          font-family: 'JetBrains Mono', monospace;
         }
 
         .grid {
@@ -100,7 +156,6 @@ export default function Home() {
           flex-wrap: wrap;
 
           max-width: 800px;
-          margin-top: 3rem;
         }
 
         .card {
@@ -118,8 +173,8 @@ export default function Home() {
         .card:hover,
         .card:focus,
         .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+          color: #2D00F7;
+          border-color: #2D00F7;
         }
 
         .card h3 {
@@ -131,10 +186,6 @@ export default function Home() {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
         }
 
         @media (max-width: 600px) {
@@ -150,11 +201,9 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: 'JetBrains Mono', monospace;
         }
-
+        
         * {
           box-sizing: border-box;
         }
