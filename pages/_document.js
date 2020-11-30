07c80c9d24408bd5e448699cbc05594ba9f94719
Joselito <eu@joseli.to>
2020-11-30 02:58:24 -0300

    feat(analytics): add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import parse from "html-react-parser";
 import { configureLanguage } from "../utils/language";
 
 class JoseDocument extends Document {
@@ -14,11 +15,14 @@ class JoseDocument extends Document {
     const { language } = this.props;
 
     return (
-      <Html lang={!!language ? language : 'en'}>
+      <Html lang={!!language ? language : "en"}>
         <Head />
         <body>
           <Main />
           <NextScript />
+          {parse(
+            '<script async defer data-website-id="a1045690-dc4f-48f2-a21f-e93fce01c7ba" src="https://a.joseli.to/umami.js"></script>'
+          )}
         </body>
       </Html>
     );
