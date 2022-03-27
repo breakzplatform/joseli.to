3aa34d371a5f61d8594dfe5daaff028d2947c639
Joselito <eu@joseli.to>
2022-03-27 16:54:52 -0300

    new plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,24 @@
 const htmlmin = require("html-minifier");
 const { DateTime } = require("luxon");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const anchorsPlugin = require('@orchidjs/eleventy-plugin-ids');
+const wordStats = require('@photogabble/eleventy-plugin-word-stats');
+const recentChanges = require('eleventy-plugin-recent-changes');
 
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(anchorsPlugin);
+  eleventyConfig.addPlugin(wordStats, {
+    output: function (stats) {
+      return `${stats.words} palavras, ${stats.minutes}`;
+    },
+  });
+  eleventyConfig.addPlugin(recentChanges);
 
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('src/_redirects');
